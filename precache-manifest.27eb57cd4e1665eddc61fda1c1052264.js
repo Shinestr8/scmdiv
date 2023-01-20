a740e6839dfb3b9203e0f067f4921dfaa39f8ada
shinestr8 <shinedownizstr8@gmail.com>
2023-01-20 19:49:21 +0100

    Updates

---
+++ precache-manifest.27eb57cd4e1665eddc61fda1c1052264.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b4c3a6c40b1ad693eb3",
+    "url": "/static/css/main.0a1b94c0.chunk.css"
+  },
+  {
+    "revision": "6b4c3a6c40b1ad693eb3",
+    "url": "/static/js/main.9a6f11de.chunk.js"
+  },
+  {
+    "revision": "a7a93d1503069d79f58d",
+    "url": "/static/js/runtime~main.629448d9.js"
+  },
+  {
+    "revision": "4260ed07662bf2ae07d6",
+    "url": "/static/js/2.a9d2c23b.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "eb88a6484b7e0adf565124c6fa813330",
+    "url": "/index.html"
+  }
+];
